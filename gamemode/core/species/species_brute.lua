defe109ca383aa64551363b3aa7725f5e48427cb
gaylord steambath <themaster1648@gmail.com>
2022-07-11 16:23:53 -0700

    asdfasgdasg

--- gamemode/core/species/species_brute.lua
+++ gamemode/core/species/species_brute.lua
@@ -5,7 +5,7 @@ CLASS.Team 					= TEAM_COVENANT
 
 CLASS.ForceTeamSpawn 		= true
 
-CLASS.BaseHealth 			= 350
+CLASS.BaseHealth 			= 450
 
 CLASS.WeaponLoadout 		= {"eternity_hands_strong"}
 CLASS.PlayerModels 			= {
